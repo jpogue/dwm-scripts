d1aba7bd438c1f29893c4f8fa352cc671ca64d5b
jstn <pogue.justin@gmail.com>
2019-01-31 19:26:45 -0500

    lemonbar status.sh

--- status.sh
+++ status.sh
@@ -1,34 +1,49 @@
+#############################################################
+#
+# Puts together a bunch of stuff and echoes it.  
+# Requires Siji font for icons and statuscolors for color
+#
+#############################################################
+
 #!/usr/local/bin/bash
-#called by startdwm.sh
-#puts together a bunch of stuff and pushes it to the statusbar. Requires Siji font for icons and statuscolors for color
 
 while true; do
 
-	# get numbers for everyting
+	# get numbers for everything
 	
 	BATTPERC="$(apm | grep -A 3 'Battery 0:' | grep life | grep -o '[0-9]\+')"
 	DISK1="$(df -h /home | grep -o '[0-9]\+%' | sed s/%//g)"
 	DISK2="$(df -h / | grep ROOT | grep -o '[0-9]\+%' | sed s/%//g)"
 	DISKNUM=$((DISK1+DISK2))
 
-       # set all of our icons
+        # Battery icon red at < 20% and orange at < 50%, otherwise white
 	if [ $BATTPERC -le 20 ]; then
-		BATTICON="$(echo -ne '\x03')$(echo -ne '\ue24a')$(echo -ne '\x01') "
+		BATTICON="$(echo -ne '%{F#FF0000}')$(echo -ne '\ue24d')$(echo -ne '%{F#FFFFFF}') " 
+	elif [ $BATTPERC -le 50 ]; then 
+		BATTICON="$(echo -ne '%{F#ff950e}')$(echo -ne '\ue251')$(echo -ne '%{F#FFFFFF}') "
 	else
-		BATTICON="$(echo -ne '\ue24a')"
+		BATTICON="$(echo -ne '%{F#FFFFFF}')$(echo -ne '\ue254') "
 	fi
 
-	DISKICON="$(echo -ne '\ue147')"
+	# Disk icon red at > 90% full and orange at > 60% full, otherwise white	
+	if [ $DISKNUM -ge 90 ]; then
+		DISKICON="$(echo -ne '%{F#FF0000}')$(echo -ne '\ue147')$(echo -ne '%{F#FFFFFF}') " 
+	elif [ $DISKNUM -ge 60 ]; then 
+		DISKICON="$(echo -ne '%{F#ff950e}')$(echo -ne '\ue147')$(echo -ne '%{F#FFFFFF}') "
+	else
+		DISKICON="$(echo -ne '%{F#FFFFFF}')$(echo -ne '\ue147') "
+	fi
+
 	CLOCKICON="$(echo -ne '\ue081')"
 	CALICON="$(echo -ne '\ue225')"
 	DWMICON="$(echo -ne '\ue241')"
 
-   	# all the stuff we're gonna send to xsetroot
+   	# Consolidate all the stuff we're gonna send to xsetroot, not necessary but makes altering the final 'echo' command simpler
 	TIME=" $CLOCKICON$(date +%I:%M\ %p) "
 	DATE=" $CALICON$(date +%h\ %d) "
 	BATT=" $BATTICON$(apm | grep -A 3 'Battery 0:' | grep life | grep -o '[0-9]\+%') "
 	DISK=" $DISKICON$DISKNUM% "
 
-	xsetroot -name " [$DISK$BATT$DATE$TIME]" 
+	echo "%{c}[ $DISK $BATT $DATE $TIME ]"
 	sleep 10
 done
